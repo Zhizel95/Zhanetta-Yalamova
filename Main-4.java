d11eca16fbf06168b1650c2a9e9c0609a949303d
Janet Ila <121127619+Zhizel95@users.noreply.github.com>
2023-01-01 22:15:54 +0300

    Add files via upload

---
+++ Main-4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+  
+  
+  {
+    int a = 39;
+    int dozens = a / 10; //3
+    int b = dozens * 10;//30
+    int units = a - b; //9
+    int sum = dozens + units;//12
+    int product = dozens*units;//27
+    System.out.println(dozens);
+    System.out.println(units);
+    System.out.println(sum);
+    System.out.println(product);
+  }
+  }
+}
