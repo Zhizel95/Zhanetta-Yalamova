585fe2ccd8ad9f3b96e00fca096b33f7f53debd2
Zhizel95 <121127619+Zhizel95@users.noreply.github.com>
2023-01-01 21:52:56 +0300

    Add files via upload

---
+++ Main-3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+  
+  int days = 234;
+  int daysInWeek = 7;
+  int week = days/ daysInWeek;
+ 
+  System.out.println(week);
+  
+  int daysInYear = 365;
+  int monthInYear  = 12;
+  int daysInMonth = daysInYear/monthInYear;
+  int month = days/ daysInMonth;
+  
+  System.out.println(month);
+  
+  
+  
+  
+  }
+}
