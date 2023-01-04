e7d54954170f8663e81a2c6d9451a6a2eae104d6
Janet Ila <121127619+Zhizel95@users.noreply.github.com>
2023-01-04 21:22:25 +0300

    Add files via upload

---
+++ Main-10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {                 
+    int circle = 387;
+    int square = 415;
+    
+    if  (circle>square)
+  {
+    System.out.println("Квадрат уместится в круге!");
+  } 
+   else
+  {
+    System.out.println("Круг уместится в квадрате!");
+  }
+}
+}
