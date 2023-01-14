b89f7ea8ae754ac143e5fa643f0e001d088fe3db
Janet Ila <121127619+Zhizel95@users.noreply.github.com>
2023-01-15 01:59:34 +0300

    Add files via upload

---
+++ Main-13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int game = sc.nextInt();
+    
+   if ( game == 3)
+    {
+    System.out.println("Выигрыш");
+    }
+    else 
+    if (game == 1)
+    {
+    System.out.println("Ничья"); 
+    }
+    else 
+    if (game == 0)
+    {
+    System.out.println("Проигрыш");
+    }
+  }
+}
